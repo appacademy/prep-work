808290c89ca5c6d521bb2f469880df7818088981
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-24 15:49:33 -0700

    fixup 02 spec

--- entry_assessment_practice/spec/02_letter_count_spec.rb
+++ entry_assessment_practice/spec/02_letter_count_spec.rb
@@ -1,20 +1,40 @@
 require '02_letter_count'
 require 'rspec'
 
+# Write a function, `letter_count(str)` that takes a string and
+# returns a hash mapping each letter to its frequency. Do not include
+# spaces.
+#
+# Difficulty: 1/5
+
 describe "#letter_count" do
-# Write a function letter_count that takes a string of words and returns a hash listing each letter and its frequency
-# 1/5
-  
-  it "should handle a simple case" do
-    letter_count("cat").should == {"c" => 1, "a" => 1, "t" => 1 }
+  it "handles a simple case" do
+    letter_count("cat").should == {
+      "c" => 1,
+      "a" => 1,
+      "t" => 1
+    }
   end
 
-  it "should handle multiple of the same letter" do
-    letter_count("moon").should == { "m" => 1, "o" => 2, "n" => 1 }
+  it "handles multiples of the same letter" do
+    letter_count("moon").should == {
+      "m" => 1,
+      "o" => 2,
+      "n" => 1
+    }
   end
 
-  it "should handle a complex case" do
-    letter_count("cats are fun").should == { "a" => 2, "c" => 1, "e"=> 1, "f" => 1, "n" => 1, "r" => 1, "s" => 1, "t" => 1, "u" => 1 }
+  it "handles a multi-word case" do
+    letter_count("cats are fun").should == {
+      "a" => 2,
+      "c" => 1,
+      "e"=> 1,
+      "f" => 1,
+      "n" => 1,
+      "r" => 1,
+      "s" => 1,
+      "t" => 1,
+      "u" => 1
+    }
   end
-  
 end
