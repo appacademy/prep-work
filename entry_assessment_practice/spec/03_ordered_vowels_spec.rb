3658a8a2090be683deadc86dbe5ae078730b9cd6
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-24 17:28:14 -0700

    fixup 03 spec

--- entry_assessment_practice/spec/03_ordered_vowels_spec.rb
+++ entry_assessment_practice/spec/03_ordered_vowels_spec.rb
@@ -1,36 +1,45 @@
 require '03_ordered_vowels'
 require 'rspec'
 
-describe "#ordered_vowel_words" do
-# Write a method ordered_vowel_words that takes a string of lowercase words and returns a string with just the words containing all their vowels (excluding "y") in alphabetical order
-# 2/5
+# Write a method, `ordered_vowel_words(str)` that takes a string of
+# lowercase words and returns a string with just the words containing
+# all their vowels (excluding "y") in alphabetical order. Vowels may
+# be repeated (`"afoot"` is an ordered vowel word).
+#
+# You will probably want a helper method, `ordered_vowel_word?(word)`
+# which returns true/false if a word's vowels are ordered.
+#
+# Difficulty: 2/5
 
-  it "should return a word that is in order" do
+describe "#ordered_vowel_words" do
+  it "returns a word that is in order" do
     ordered_vowel_words("amends").should == "amends"
   end
 
-  it "should not return a word that is not in order" do
+  it "does not return a word that is not in order" do
     ordered_vowel_words("complicated").should_not == "complicated"
   end
 
-  it "should be okay with double vowels" do
+  it "handle double vowels" do
     ordered_vowel_words("afoot").should == "afoot"
   end
 
-  it "should handle a word with a single vowel" do
+  it "handles a word with a single vowel" do
     ordered_vowel_words("ham").should == "ham"
   end
 
-  it "should handle a word with a single letter" do
+  it "handles a word with a single letter" do
     ordered_vowel_words("o").should == "o"
   end
 
-  it "should ignore the letter y" do
+  it "ignores the letter y" do
     ordered_vowel_words("tamely").should == "tamely"
   end
 
-  it "should properly process a mixed group of words" do
-    ordered_vowel_words("this is a test of the vowel ordering system").should == "this is a test of the system"
-  end
+  it "processes a string with several words" do
+    phrase = "this is a test of the vowel ordering system"
+    result = "this is a test of the system"
 
+    ordered_vowel_words(phrase).should == result
+  end
 end
