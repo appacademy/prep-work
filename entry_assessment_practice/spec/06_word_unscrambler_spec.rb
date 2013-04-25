2e4e6d19a7ee4809e78e4bfa3706030563e370ab
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-24 21:22:11 -0700

    fixup spec #06

--- entry_assessment_practice/spec/06_word_unscrambler_spec.rb
+++ entry_assessment_practice/spec/06_word_unscrambler_spec.rb
@@ -1,10 +1,17 @@
 require 'rspec'
 require '06_word_unscrambler'
 
-describe "#word_unscrambler" do
-# Write a function word_unscrambler that takes two inputs: a scrambled word and a dictionary of real words.  Your program must then output all words that our scrambled word can unscramble to.
-# 3/5
+# Write a function word_unscrambler that takes two inputs: a scrambled
+# word and a dictionary of real words.  Your program must then output
+# all words that our scrambled word can unscramble to.
+#
+# Hint: To see if a string `s1` is an anagram of `s2`, split both
+# strings into arrays of letters. Sort the two arrays. If they are
+# equal, then they are anagrams.
+#
+# Difficulty: 3/5
 
+describe "#word_unscrambler" do
   it "should handle a simple word" do
     word_unscrambler("cat", ["tac"]).should == ["tac"]
   end
@@ -24,5 +31,4 @@ describe "#word_unscrambler" do
   it "should handle multiple successful cases" do
     word_unscrambler("turn", ["numb", "turn", "runt", "nurt"]).should == ["turn", "runt", "nurt"]
   end
-
 end
