9f864895bdcb7b6ba7dce74707a1184e81f364d7
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-24 21:20:03 -0700

    add #09 solution

---
+++ entry_assessment_practice/lib/09_scientific_notation.rb
@@ -0,0 +1,18 @@
+def sci_not(num)
+  num = num.to_f
+  pow = 0
+  while (num > 10)
+    num /= 10
+    pow += 1
+  end
+
+  str = ""
+  str << num.round.to_s
+  str << "."
+  str << ((num * 10).round % 10).to_s
+  str << ((num * 100).round % 10).to_s
+  str << "E"
+  str << pow.to_s
+
+  str
+end
