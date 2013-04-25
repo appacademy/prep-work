3fcc96b8c56df6d2f3ca82f36bdd1ae1838add98
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-24 18:16:26 -0700

    fixup spec #08

--- entry_assessment_practice/spec/08_bubble_sort_spec.rb
+++ entry_assessment_practice/spec/08_bubble_sort_spec.rb
@@ -1,27 +1,22 @@
 require '08_bubble_sort'
 require 'rspec'
 
-describe "#bubble_sort" do
-# Write a function Array.bubble_sort which will sort an array of integers using the "bubble sort" methodology. (http://en.wikipedia.org/wiki/Bubble_sort)
-# 3/5
+# Write a function `bubble_sort(arr)` which will sort an array of
+# integers using the "bubble sort"
+# methodology. (http://en.wikipedia.org/wiki/Bubble_sort)
+#
+# Difficulty: 3/5
 
+describe "#bubble_sort" do
   it "works with an empty array" do
-    [].bubble_sort.should == []
+    bubble_sort([]).should == []
   end
 
   it "works with an array of one item" do
-    [1].bubble_sort.should == [1]
+    bubble_sort([1]).should == [1]
   end
 
   it "sorts numbers" do
-    [5, 4, 3, 2, 1].bubble_sort.should == [1, 2, 3, 4, 5]
+    bubble_sort([5, 4, 3, 2, 1]).should == [1, 2, 3, 4, 5]
   end
-
-  it "does not modify original" do
-    original = [5, 4, 3, 2, 1]
-    duped_original = original.dup
-    duped_original.bubble_sort
-    duped_original.should == original
-  end
-
 end
