4ec8eb73d6d5f8697d707ddfca1b4e464f968f49
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-24 21:19:53 -0700

    fix #09 spec

--- entry_assessment_practice/spec/09_scientific_notation_spec.rb
+++ entry_assessment_practice/spec/09_scientific_notation_spec.rb
@@ -1,40 +1,42 @@
 require 'rspec'
 require '09_scientific_notation'
 
-describe "#sci_not" do
-# Write a method sci_not that converts a positive integer or positive decimal into scientific notation rounded to 2 decimal places (e.g. sci_not(.2345) == "2.35E-1"  (use "E" instead of "10^"))
-# 3/5
-  
-  it "should convert a single-digit int" do
-    sci_not(3).should == "3.0E0"
-  end
+# Write a method `sci_not(num) that converts a positive number > 1
+# into scientific notation rounded to 2 decimal places
+# (e.g. `sci_not(234.5) == "2.35E2"` (use "E" instead of "10^")).
+#
+# You may want to convert the integer input to a float: you can use
+# `1.to_f`.
+#
+# My solution:
 
-  it "should convert a single-digit decimal" do
-    sci_not(1.2345).should == "1.23E0"
+# (1) Repeatedly divide `num` by 10 until it is less than < 10. Record
+#     how many times you did this.
+# (2) Set `str = ""`
+# (3) Tack on the `num` rounded to an integer (`#round`).
+# (4) Tack on a "."
+# (5) Multiple the number by 10; chop off the everything beyond the
+#     tens part (check out the `%` remainder operator). Round to an
+#     integer and tack this on.
+# (6) Repeat for the second decimal digit.
+# (7) Tack on "E" and the power.
+#
+# Difficulty: 4/5
+
+describe "#sci_not" do
+  it "should convert a single-digit int" do
+    sci_not(3).should == "3.00E0"
   end
 
   it "should convert a simple integer" do
-    sci_not(11).should == "1.1E1"
+    sci_not(11).should == "1.10E1"
   end
 
   it "should convert an integer" do
     sci_not(12345).should == "1.23E4"
   end
 
-  it "should convert a simple decimal" do
-    sci_not(0.12345).should == "1.23E-1"
-  end
-
-  it "should convert a complex decimal" do
-    sci_not(0.000123).should == "1.23E-4"
-  end
-
   it "should convert a round-up situation" do
-    sci_not(1.249).should == "1.25E0"
+    sci_not(1249).should == "1.25E3"
   end
-
-  it "should convert a mixed number" do
-    sci_not(12345.6789).should == "1.23E4"
-  end
-
 end
