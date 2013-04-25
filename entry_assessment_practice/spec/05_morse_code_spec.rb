53e3839568e4be2daefad495ca9f26ee0b4ac81b
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-24 17:57:16 -0700

    fixup spec #05

--- entry_assessment_practice/spec/05_morse_code_spec.rb
+++ entry_assessment_practice/spec/05_morse_code_spec.rb
@@ -1,10 +1,20 @@
 require '05_morse_code'
 require 'rspec'
 
-describe "#morse_encode" do
-# Build a function morse_encode that takes in a word (no numbers or punctuation) and outputs the morse code for it.  http://www.w1wc.com/pdf_files/international_morse_code.pdf  Put two spaces between words and one space between letters.  
-# 2/5
+# Build a function, `morse_encode(str)` that takes in a string (no
+# numbers or punctuation) and outputs the morse code for it. See
+# http://en.wikipedia.org/wiki/Morse_code. Put two spaces between
+# words and one space between letters.
+#
+# You'll have to type in morse code: I'd use a hash to map letters to
+# codes. Don't worry about numbers.
+#
+# I wrote a helper method `morse_encode_word(word)` that handled a
+# single word.
+#
+# Difficulty: 2/5
 
+describe "#morse_encode" do
   it "should do a simple letter" do
     morse_encode("q").should == "--.-"
   end
@@ -16,5 +26,4 @@ describe "#morse_encode" do
   it "should handle a phrase" do
     morse_encode("cat in hat").should == "-.-. .- -  .. -.  .... .- -"
   end
-
 end
