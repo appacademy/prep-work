4db42d48125a346faae9a9da909d07da46a02384
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-24 17:34:23 -0700

    fixup spec #04

--- entry_assessment_practice/spec/04_wonky_coins_spec.rb
+++ entry_assessment_practice/spec/04_wonky_coins_spec.rb
@@ -1,18 +1,32 @@
-# -*- coding: utf-8 -*-
 require '04_wonky_coins'
 require 'rspec'
 
-# -*- coding: utf-8 -*-
-describe "#wonky_coins" do
-# Catsylvanian money is a strange thing… they have a coin for every denomination (including zero).  A wonky change machine in Catsylvania takes any coin of value N and returns 3 new coins, valued at N/2, N/3 and N/4 (rounded down).  Write a method wonky_coins that returns the number of coins you are left with if you take all non-zero coins and keep feeding them back into the machine until you are left with only zero-value coins.
-# 2/5
+# Catsylvanian money is a strange thing: they have a coin for every
+# denomination (including zero!). A wonky change machine in
+# Catsylvania takes any coin of value N and returns 3 new coins,
+# valued at N/2, N/3 and N/4 (rounding down).
+#
+# Write a method `wonky_coins(n)` that returns the number of coins you
+# are left with if you take all non-zero coins and keep feeding them
+# back into the machine until you are left with only zero-value coins.
+#
+# Difficulty: 3/5
 
-  it "should handle a simple case" do
+describe "#wonky_coins" do
+  it "handles a simple case" do
     wonky_coins(1).should == 3
   end
 
-  it "should handle a larger case" do
-    wonky_coins(5).should == 11 # ( 2, 1, 1 => 1, 0, 0, 0, 0, 0, 0, 0, 0, => 0,0,0,0,0,0,0,0,0,0,0, or 11 coins.
+  it "handles a larger case" do
+    wonky_coins(5).should == 11
+    # 11
+    # => [2, 1, 1]
+    # => [[1, 0, 0], [0, 0, 0], [0, 0, 0]]
+    # => [[[0, 0, 0], 0, 0], [0, 0, 0], [0, 0, 0]]
+  end
+
+  it "handles being given the zero coin" do
+    wonky_coins(0).should == 1
   end
 
 end
