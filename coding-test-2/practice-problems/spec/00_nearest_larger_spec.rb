664d473d3752699aa5cc3987db47ed53f21c7171
sarahquigley <sarah@sarahquigley.net>
2013-05-02 20:27:03 -0700

    Bug fix: nearest_larger solution failed in some cases
    nearest_larger sample solution failed on inputs where the nearest_larger
    number was at a distance > 1 from the input index, returned nil in error

--- coding-test-2/practice-problems/spec/00_nearest_larger_spec.rb
+++ coding-test-2/practice-problems/spec/00_nearest_larger_spec.rb
@@ -31,6 +31,14 @@ describe "#nearest_larger" do
     nearest_larger([2,6,4,6], 2).should == 1
   end
 
+  it "handles a case with an answer > 1 distance to the left" do
+    nearest_larger([8,2,4,3], 2).should == 0
+  end
+  
+  it "handles a case with an answer > 1 distance to the right" do
+    nearest_larger([2,4,3,8], 1).should == 3
+  end
+
   it "should return nil if no larger number is found" do
     nearest_larger( [2, 6, 4, 8], 3).should == nil
   end
