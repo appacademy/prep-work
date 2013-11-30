f770bb45d678e018cb7f379dae1aa8adeaa73c29
tlee418 <tlee418@ucla.edu>
2013-11-30 04:33:30 -0800

    Update 06_word_unscrambler.rb

--- coding-test-2/practice-problems/solutions/06_word_unscrambler.rb
+++ coding-test-2/practice-problems/solutions/06_word_unscrambler.rb
@@ -1,11 +1,5 @@
 def word_unscrambler(str, words)
-  str_letters = str.split("").sort
+  anagrams = words.select { |word| word.split("").sort == str.split("").sort}
 
-  anagrams = []
-  words.select do |word|
-    word_letters = word.split("").sort
-    anagrams << word if str_letters == word_letters
-  end
-
-  anagrams
+  puts anagrams
 end
