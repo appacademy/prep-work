8f3409211778d5821669d8fa0b6d0c3b5c193383
Ned Ruggeri <ruggeri@self-loop.com>
2014-02-16 09:21:27 -0800

    demo simpler solution to unscrambler

--- coding-test-2/practice-problems/solutions/06_word_unscrambler.rb
+++ coding-test-2/practice-problems/solutions/06_word_unscrambler.rb
@@ -2,10 +2,19 @@ def word_unscrambler(str, words)
   str_letters = str.split("").sort
 
   anagrams = []
-  words.select do |word|
+  words.each do |word|
     word_letters = word.split("").sort
     anagrams << word if str_letters == word_letters
   end
 
   anagrams
 end
+
+# This could also be written as a two-liner:
+#
+# def word_unscrambler(str, words)
+#   str_letters = str.split("").sort
+#   words.select { |word| str_letters == word.split("").sort }
+# end
+#
+# I've chosen to write it more explicitly for clarity to beginners.
