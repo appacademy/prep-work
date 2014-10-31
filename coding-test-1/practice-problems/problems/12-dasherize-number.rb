30d35d6ed56eec439fe6cdf75dded93944d11557
th11 <th11@users.noreply.github.com>
2014-10-30 21:25:13 -1000

    Problem 12 - clarify directions
    Clarify that there should not be a dash at beginning or end of string.

--- coding-test-1/practice-problems/problems/12-dasherize-number.rb
+++ coding-test-1/practice-problems/problems/12-dasherize-number.rb
@@ -1,5 +1,6 @@
 # Write a method that takes in a number and returns a string, placing
-# a single dash before and after each odd digit.
+# a single dash before and after each odd digit. There is one exception to this rule, 
+# the string should not start or end with a dash.
 #
 # You may wish to use the `%` modulo operation; you can see if a number
 # is even if it has zero remainder when divided by two.
