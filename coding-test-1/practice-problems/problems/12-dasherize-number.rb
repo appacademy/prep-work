bf6f15ffe776d5e8852dc56b71043b2084f7ab3d
Ned Ruggeri <ruggeri@self-loop.com>
2014-10-31 16:43:02 -0700

    clarify dasherize number instructions
    Thanks @th11! Closes #43

--- coding-test-1/practice-problems/problems/12-dasherize-number.rb
+++ coding-test-1/practice-problems/problems/12-dasherize-number.rb
@@ -1,5 +1,6 @@
 # Write a method that takes in a number and returns a string, placing
-# a single dash before and after each odd digit.
+# a single dash before and after each odd digit. There is one
+# exception: don't start or end the string with a dash.
 #
 # You may wish to use the `%` modulo operation; you can see if a number
 # is even if it has zero remainder when divided by two.
