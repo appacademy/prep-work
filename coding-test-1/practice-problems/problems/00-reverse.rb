653a1e1d3b6792c4edc67cc2d927ef3041bc9ef5
Ned Ruggeri <ruggeri@self-loop.com>
2015-01-02 20:14:36 -0800

    add note about String#reverse: don't use it!

--- coding-test-1/practice-problems/problems/00-reverse.rb
+++ coding-test-1/practice-problems/problems/00-reverse.rb
@@ -1,6 +1,8 @@
 # Write a method that will take a string as input, and return a new
 # string with the same letters in reverse order.
 #
+# Don't use String's reverse method; that would be too simple.
+#
 # Difficulty: easy.
 
 def reverse(string)
