d8aa889c999f29613112332c59985c27de296087
Vee Trairattanapa <happierv@gmail.com>
2015-04-05 23:07:34 -0700

    10-third-greatest

---
+++ coding-test-1/practice-problems/problems-python/10-third-greatest.py
@@ -0,0 +1,15 @@
+# Write a method that takes an array of numbers in. Your method should
+# return the third greatest number in the array. You may assume that
+# the array has at least three numbers in it.
+#
+# Difficulty: medium.
+
+def third_greatest(nums):
+  pass
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+print 'third_greatest([5, 3, 7]) == 3: ' + str(third_greatest([5, 3, 7]) == 3)
+print 'third_greatest([5, 3, 7, 4]) == 4: ' + str(third_greatest([5, 3, 7, 4]) == 4)
+print 'third_greatest([2, 3, 7, 4]) == 3: ' + str(third_greatest([2, 3, 7, 4]) == 3)
