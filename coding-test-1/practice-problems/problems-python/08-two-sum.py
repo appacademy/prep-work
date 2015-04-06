08363d7b8923e45f770d5fcfd0f3b7974aef5b4e
Vee Trairattanapa <happierv@gmail.com>
2015-04-05 23:09:38 -0700

    08-two-sum.py edit comment

--- coding-test-1/practice-problems/problems-python/08-two-sum.py
+++ coding-test-1/practice-problems/problems-python/08-two-sum.py
@@ -1,6 +1,6 @@
 # Write a method that takes an array of numbers. If a pair of numbers
 # in the array sums to zero, return the positions of those two numbers.
-# If no pair of numbers sums to zero, return `nil`.
+# If no pair of numbers sums to zero, return `None`.
 #
 # Difficulty: medium.
 
