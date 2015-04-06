7e84699eddb2c52b812e56ec739378572cfdd857
Vee Trairattanapa <happierv@gmail.com>
2015-04-05 21:16:48 -0700

    02-longest-word.py 03-sum-nums.py

---
+++ coding-test-1/practice-problems/problems-python/03-sum-nums.py
@@ -0,0 +1,16 @@
+# Write a method that takes in an integer `num` and returns the sum of
+# all integers between zero and num, up to and including `num`.
+#
+# Difficulty: easy.
+
+def sum_nums(num):
+	pass
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+print 'sum_nums(1) == 1: ' + str(sum_nums(1) == 1)
+print 'sum_nums(2) == 3: ' + str(sum_nums(2) == 3)
+print 'sum_nums(3) == 6: ' + str(sum_nums(3) == 6)
+print 'sum_nums(4) == 10: ' + str(sum_nums(4) == 10)
+print 'sum_nums(5) == 15: ' + str(sum_nums(5) == 15)
