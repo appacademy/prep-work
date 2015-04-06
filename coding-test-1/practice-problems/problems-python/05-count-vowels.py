714e4fbd03d05ff0bcb11e3cb8780458feb314ff
Vee Trairattanapa <happierv@gmail.com>
2015-04-05 21:32:56 -0700

    05-count-vowels.py

---
+++ coding-test-1/practice-problems/problems-python/05-count-vowels.py
@@ -0,0 +1,16 @@
+# Write a method that takes a string and returns the number of vowels
+# in the string. You may assume that all the letters are lower cased.
+# You can treat "y" as a consonant.
+#
+# Difficulty: easy.
+
+def count_vowels(string):
+  pass
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+print 'count_vowels("abcd") == 1: ' + str(count_vowels('abcd') == 1)
+print 'count_vowels("color") == 2: ' + str(count_vowels('color') == 2)
+print 'count_vowels("colour") == 3: ' + str(count_vowels('colour') == 3)
+print 'count_vowels("cecilia") == 4: ' + str(count_vowels('cecilia') == 4)
